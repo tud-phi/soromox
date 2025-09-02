268669e13a9e8e46b0ffbc00cd1415d54fe31b7e
Michele-Martini <martini.michele15@gmail.com>
2025-09-02 22:50:32 +0200

    modified test cases and added docstrings.

--- tests/test_tendon_actuated_pcs.py
+++ tests/test_tendon_actuated_pcs.py
@@ -12,19 +12,35 @@ from soromox.utils.tolerance import Tolerance
 
 def test_actuation_matrix_pcs():
     """
-    Test ...
+    Test the methods responsible for the computation of the actuation matrix of the
+    tendon actuated Piecewise Constant Strain class.
     """
-    def createRobot(num_segments, spec_params, tendon_params):
-        rho = 1070 * jnp.ones((num_segments,))  # Volumetric density of Dragon Skin 20 [kg/m^3]
+
+    def createRobot(segment_lengths, tendon_params):
+        """
+        Creates an object of the class TendonActuatedPCS with the specified segments' length
+        and tendon routing parameters.
+
+        Args:
+            spec_params (Array): lengths of the segments of the robot (num_segments, )
+            tendon_params (Dict[str, Array]): routing parameters of the given tendon
+
+        Returns:
+            robot (TendonActuatedPCS): object representing the soft robot
+        """
+        num_segments = segment_lengths.shape[0]
+        rho = 1070 * jnp.ones(
+            (num_segments,)
+        )  # Volumetric density of Dragon Skin 20 [kg/m^3]
         params = {
             "p0": jnp.zeros(6),  # Initial position and orientation
-            "r": 1. * jnp.ones((num_segments,)),  # default: 2e-2
+            "r": 1.0 * jnp.ones((num_segments,)),  # default: 2e-2
             "rho": rho,
             "g": jnp.array([0.0, 0.0, 9.81]),  # Gravity vector [m/s^2]
             "E": 2e3 * jnp.ones((num_segments,)),  # Elastic modulus [Pa]
             "G": 1e3 * jnp.ones((num_segments,)),  # Shear modulus [Pa]
         }
-        params.update(spec_params)#["L"] = 1e-1 * jnp.ones((num_segments,))
+        params["L"] = segment_lengths
         params["D"] = 1e-3 * jnp.diag(
             (
                 jnp.repeat(
@@ -38,41 +54,82 @@ def test_actuation_matrix_pcs():
             num_segments=num_segments,
             params=params,
             order_gauss=5,
-            # strain_selector=strain_selector,
-            # xi_ref=xi_ref,
             tendon_routing_params=tendon_params,
         )
-        
-        return robot, jnp.zeros(6*num_segments)
-    
-    def A_special_case(spec_params, tendon_params):
-        l = jnp.sum(spec_params["L"])
-        ry, rz, my, mz = tendon_params["ry"][0], tendon_params["rz"][0], tendon_params["my"][0], tendon_params["mz"][0]
-        A = jnp.array([
-            l*(-my*rz + mz*ry),
-            l**2*mz/2 + l*rz,
-            -l**2*my/2 - l*ry,
-            l,
-            l*my,
-            l*mz
-        ])
+
+        return robot
+
+    def reference_actuation_matrix(tendon_params, l_tot):
+        """
+        Compute the actuation matrix of a soft robot of length l_tot w.r.t. one
+        linear tendon at the straight configuration corresponding to vector state
+        q0 = zeros(6*num_segments), considered as reference.
+        This configuration represents the rest (stress-free) shape of the robot
+        and it is easily tractable analitically.
+
+        Args:
+            tendon_params (Dict[str, Array]): routing parameters of the given tendon
+            l_tot (Array): total length of the robot
+
+        Returns:
+            A (Array): actuation matrix at straight configuration
+        """
+        ry, rz, my, mz = (
+            tendon_params["ry"][0],
+            tendon_params["rz"][0],
+            tendon_params["my"][0],
+            tendon_params["mz"][0],
+        )
+        A = jnp.array(
+            [
+                l_tot * (-my * rz + mz * ry),
+                l_tot**2 * mz / 2 + l_tot * rz,
+                -(l_tot**2) * my / 2 - l_tot * ry,
+                l_tot,
+                l_tot * my,
+                l_tot * mz,
+            ]
+        )
 
         return A / jnp.sqrt(my**2 + mz**2 + 1)
-    
-    def actuation_basis(tendon_params, xi, s):
-        ry, rz, my, mz = tendon_params["ry"][0], tendon_params["rz"][0], tendon_params["my"][0], tendon_params["mz"][0]
-        xi_1, xi_2, xi_3, xi_4, xi_5, xi_6 = xi[0], xi[1], xi[2], xi[3], xi[4], xi[5]
-        Phi_a = jnp.array([
-            (my*s + ry)*(mz + xi_1*(my*s + ry) + xi_6) + (-mz*s - rz)*(my - xi_1*(mz*s + rz) + xi_5),
-            (mz*s + rz)*(xi_2*(mz*s + rz) - xi_3*(my*s + ry) + xi_4),
-            (-my*s - ry)*(xi_2*(mz*s + rz) - xi_3*(my*s + ry) + xi_4),
-            xi_2*(mz*s + rz) - xi_3*(my*s + ry) + xi_4,
-            my - xi_1*(mz*s + rz) + xi_5,
-            mz + xi_1*(my*s + ry) + xi_6
-        ])
-        norm = jnp.sqrt((my - xi_1*(mz*s + rz) + xi_5)**2 + (mz + xi_1*(my*s + ry) + xi_6)**2 + (xi_2*(mz*s + rz) - xi_3*(my*s + ry) + xi_4)**2)
-        return Phi_a / norm
 
+    def reference_actuation_basis(tendon_params, q, s):
+        """
+        Computes the vector representing the actuation basis of the given tendon
+        at configuration q (assuming the strain basis to be the identity) and at
+        abscissa point s.
+
+        Args:
+            tendon_params (Dict[str, Array]): routing parameters of the given tendon
+            q (Array): strains of the robot made by one single segment (6,)
+            s (float): abscissa point ()
+
+        Returns:
+            Phi_a (Array): actuation basis of the tendon at q, s (6,)
+        """
+        xi = jnp.eye(q.shape[0]) @ q + xi_ref
+        ry, rz, my, mz = (
+            tendon_params["ry"][0],
+            tendon_params["rz"][0],
+            tendon_params["my"][0],
+            tendon_params["mz"][0],
+        )
+        xi_1, xi_2, xi_3, xi_4, xi_5, xi_6 = xi[0], xi[1], xi[2], xi[3], xi[4], xi[5]
+        Phi_a = jnp.array(
+            [
+                (my * s + ry) * (mz + xi_1 * (my * s + ry) + xi_6)
+                + (-mz * s - rz) * (my - xi_1 * (mz * s + rz) + xi_5),
+                (mz * s + rz) * (xi_2 * (mz * s + rz) - xi_3 * (my * s + ry) + xi_4),
+                (-my * s - ry) * (xi_2 * (mz * s + rz) - xi_3 * (my * s + ry) + xi_4),
+                xi_2 * (mz * s + rz) - xi_3 * (my * s + ry) + xi_4,
+                my - xi_1 * (mz * s + rz) + xi_5,
+                mz + xi_1 * (my * s + ry) + xi_6,
+            ]
+        )
+        norm = jnp.linalg.norm(Phi_a[3:])
+        Phi_a = Phi_a / norm
+
+        return Phi_a
 
     # ========================================
     # Test of the functions
@@ -83,84 +140,105 @@ def test_actuation_matrix_pcs():
 
     test_cases = [
         (
-            1,
-            {"L": jnp.array([0.5])},
-            {"ry": jnp.array([0.1]), "rz": jnp.array([0.05]), "my": jnp.array([0.]), "mz": jnp.array([0.]), 'idx_seg_att': jnp.array([0])},
-            A_special_case({"L": jnp.array([0.5])},
-                           {"ry": jnp.array([0.1]), "rz": jnp.array([0.05]), "my": jnp.array([0.]), "mz": jnp.array([0.]), 'idx_seg_att': jnp.array([0])})
+            jnp.array([0.5]),
+            {
+                "ry": jnp.array([0.1]),
+                "rz": jnp.array([0.05]),
+                "my": jnp.array([0.0]),
+                "mz": jnp.array([0.0]),
+                "idx_seg_att": jnp.array([0]),
+            },
         ),
         (
-            2,
-            {"L": jnp.array([0.5, 0.6])},
-            {"ry": jnp.array([0.15]), "rz": jnp.array([0.]), "my": jnp.array([0.01]), "mz": jnp.array([0.01]), 'idx_seg_att': jnp.array([1])},
-            A_special_case({"L": jnp.array([0.5, 0.6])},
-                           {"ry": jnp.array([0.15]), "rz": jnp.array([0.]), "my": jnp.array([0.01]), "mz": jnp.array([0.01]), 'idx_seg_att': jnp.array([1])})
+            jnp.array([0.5, 0.6]),
+            {
+                "ry": jnp.array([0.15]),
+                "rz": jnp.array([0.0]),
+                "my": jnp.array([0.01]),
+                "mz": jnp.array([0.01]),
+                "idx_seg_att": jnp.array([1]),
+            },
         ),
         (
-            3,
-            {"L": jnp.array([0.5, 0.6, 0.7])},
-            {"ry": jnp.array([0.1]), "rz": jnp.array([-0.05]), "my": jnp.array([0.01]), "mz": jnp.array([-0.05]), 'idx_seg_att': jnp.array([2])},
-            A_special_case({"L": jnp.array([0.5, 0.6, 0.7])},
-                           {"ry": jnp.array([0.1]), "rz": jnp.array([-0.05]), "my": jnp.array([0.01]), "mz": jnp.array([-0.05]), 'idx_seg_att': jnp.array([2])})
+            jnp.array([0.5, 0.6, 0.7]),
+            {
+                "ry": jnp.array([0.1]),
+                "rz": jnp.array([-0.05]),
+                "my": jnp.array([0.01]),
+                "mz": jnp.array([-0.05]),
+                "idx_seg_att": jnp.array([2]),
+            },
         ),
     ]
 
-    for num_segments, spec_pars, tendon_params, expected in test_cases:
-        robot, q0 = createRobot(num_segments, spec_pars, tendon_params)
-        a = robot.actuation_matrix(q0)
-        nq = a.shape[0] * a.shape[1]
-        n1 = int(nq / 6)
-        a = a.reshape((6, n1), order='F')
-        a = jnp.sum(a, axis=1)
-        assert not jnp.isnan(a).any(), "Actuation matrix contains NaN!"
-        assert_allclose(a, expected, rtol=Tolerance.rtol(), atol=Tolerance.atol())
+    for segment_lengths, tendon_params in test_cases:
+        robot = createRobot(segment_lengths, tendon_params)
+        num_segments = segment_lengths.shape[0]
+        q0 = jnp.zeros(6 * num_segments)
+        target_actuation_matrix = robot.actuation_matrix(q0).reshape(
+            (6, num_segments), order="F"
+        )
+        target_actuation_matrix = jnp.sum(target_actuation_matrix, axis=1)
+        expected_actuation_matrix = reference_actuation_matrix(
+            tendon_params, jnp.sum(segment_lengths)
+        )
+        assert not jnp.isnan(target_actuation_matrix).any(), (
+            "Actuation matrix contains NaN!"
+        )
+        assert_allclose(
+            target_actuation_matrix,
+            expected_actuation_matrix,
+            rtol=Tolerance.rtol(),
+            atol=Tolerance.atol(),
+        )
         print("[Valid test]\n")
 
     # test actuation basis
     print("\nTesting actuation basis... ------------------------")
-    pars = {"ry": jnp.array([0.1]), "rz": jnp.array([-0.05]), "my": jnp.array([0.01]), "mz": jnp.array([-0.05]), 'idx_seg_att': jnp.array([0])}
-    robot, _ = createRobot(1,
-                           {"L": jnp.array([0.5])},
-                           pars,
-                          )
-    xi_ref = jnp.array([0.,0.,0.,1.,0.,0.])
-    
+    tendon_params = {
+        "ry": jnp.array([0.1]),
+        "rz": jnp.array([-0.05]),
+        "my": jnp.array([0.01]),
+        "mz": jnp.array([-0.05]),
+        "idx_seg_att": jnp.array([0]),
+    }
+    robot = createRobot(jnp.array([0.5]), tendon_params)
+    xi_ref = jnp.array([0.0, 0.0, 0.0, 1.0, 0.0, 0.0])
+
     test_cases = [
         (
             jnp.zeros_like(xi_ref),
-            robot.L_cum[-1]*0.7,
-            actuation_basis(pars, xi_ref, robot.L_cum[-1]*0.7)
+            robot.L_cum[-1] * 0.25,
         ),
-
         (
-            jnp.array([0., jnp.pi*3, jnp.pi*2, 0.1, 0.2, 0.]),
-            robot.L_cum[-1]*0.7,
-            actuation_basis(pars, jnp.array([0., jnp.pi*3, jnp.pi*2, 0.1, 0.2, 0.]) + xi_ref, robot.L_cum[-1]*0.7)
+            jnp.array([0.0, jnp.pi * 3, jnp.pi * 2, 0.1, 0.2, 0.0]),
+            robot.L_cum[-1] * 0.25,
         ),
-
         (
             jnp.zeros_like(xi_ref),
-            robot.L_cum[-1]*0.25,
-            actuation_basis(pars, xi_ref, robot.L_cum[-1]*0.25)
+            robot.L_cum[-1] * 0.7,
         ),
-
         (
-            jnp.array([0., jnp.pi*3, jnp.pi*2, 0.1, 0.2, 0.]),
-            robot.L_cum[-1]*0.25,
-            actuation_basis(pars, jnp.array([0., jnp.pi*3, jnp.pi*2, 0.1, 0.2, 0.]) + xi_ref, robot.L_cum[-1]*0.25)
+            jnp.array([0.0, jnp.pi * 3, jnp.pi * 2, 0.1, 0.2, 0.0]),
+            robot.L_cum[-1] * 0.7,
         ),
-
-
     ]
 
-    for q, s, expected in test_cases:
-        phi_a = robot._local_actuation_basis(q, s).squeeze()
-        assert not jnp.isnan(phi_a).any(), "Actuation matrix contains NaN!"
-        assert_allclose(phi_a, expected, rtol=Tolerance.rtol(), atol=Tolerance.atol())
+    for q, s in test_cases:
+        target_actuation_basis = robot._local_actuation_basis(q, s).squeeze()
+        expected_actuation_basis = reference_actuation_basis(tendon_params, q, s)
+        assert not jnp.isnan(target_actuation_basis).any(), (
+            "Actuation basis contains NaN!"
+        )
+        assert_allclose(
+            target_actuation_basis,
+            expected_actuation_basis,
+            rtol=Tolerance.rtol(),
+            atol=Tolerance.atol(),
+        )
         print("[Valid test]\n")
 
 
-
 if __name__ == "__main__":
-    print("Running tests for tendon actuated Planar Constant Strain...")
+    print("Running tests for tendon actuated Piecewise Constant Strain...")
     test_actuation_matrix_pcs()
