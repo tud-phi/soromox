dd1440479ba9c829f9b65bccb0c900ecd1b0d6b8
Maximilian Stölzle <maximilian@stoelzle.ch>
2025-09-15 10:10:31 +0200

    Improve planar pcs rendering function

--- src/soromox/rendering/planar_pcs/opencv_renderer.py
+++ src/soromox/rendering/planar_pcs/opencv_renderer.py
@@ -13,7 +13,8 @@ def render_planar_pcs(
     width: int,
     height: int,
     origin_uv: Optional[Tuple] = None,
-    line_thickness: int = 2,
+    length_scale: float = 1.6,
+    backbone_thickness: Optional[int] = None,
     num_points: int = 50,
 ) -> onp.ndarray:
     """
@@ -23,15 +24,23 @@ def render_planar_pcs(
         q: An array of joint angles.
         width: The width (i.e. number of horizontal pixels) of the rendered image.
         height: The height (i.e. number of vertical pixels) of the rendered image.
-        line_thickness: The thickness of the rendered lines in pixels.
+        length_scale: The length scale of the robot in the image. The robot will occupy height/length_scale in pixels.
+        origin_uv: The (u, v) pixel coordinates of the robot base. If None, set to the center of the image.
+        backbone_thickness: The thickness of the rendered lines in pixels. If None, set it according to the robot diameter.
         num_points: The number of points used for discretizing the backbone curve.
     Returns:
         img: A numpy array of shape (width, height, 3) containing the rendered image.
     """
     # plotting in OpenCV
     h, w = height, width  # img height and width
-    ppm = h / (1.6 * jnp.sum(robot.L))  # pixel per meter
+    ppm = h / (length_scale * jnp.sum(robot.L))  # pixel per meter
+    base_color = (0, 255, 0)  # green base_color in BGR
     robot_color = (0, 0, 0)  # black robot_color in BGR
+
+    if backbone_thickness is None:
+        backbone_thicknesses = jnp.clip((ppm * robot.r).astype(jnp.int32), min=1, max=None)
+    else:
+        backbone_thicknesses = jnp.ones((robot.num_segments,)) * backbone_thickness
 
     # in uv pixel coordinates
     if origin_uv is None:
@@ -49,19 +58,28 @@ def render_planar_pcs(
     # poses along the robot of shape (3, N)
     chi_ps = batched_forward_kinematics_fn(q, s_ps)
 
+    # initialize image 
     img = 255 * onp.ones((h, w, 3), dtype=jnp.uint8)  # initialize background to white
+
+    # plot the base
+    cv2.circle(img, origin_uv, int(1.5 * robot.r.mean().item() * ppm), base_color, -1)
+
     # transform robot poses to pixel coordinates
     # should be of shape (N, 2)
-    curve = onp.array((chi_ps[:2, :].T * ppm), dtype=onp.int32)
+    curve = onp.array((chi_ps[1:3, :].T * ppm), dtype=onp.int32)
     # invert the v pixel coordinate
     curve[:, 1] = -curve[:, 1]
 
-    cv2.polylines(
-        img,
-        [origin_uv + curve],
-        isClosed=False,
-        color=robot_color,
-        thickness=line_thickness,
-    )
+    # plot each segment 
+    L_cum = jnp.concatenate((jnp.array([0]), jnp.cumsum(robot.L)))
+    for segment_idx in range(robot.num_segments):
+        curve_selector = (s_ps >= L_cum[segment_idx]) & (s_ps < L_cum[segment_idx + 1])
+        cv2.polylines(
+            img,
+            [origin_uv + curve[curve_selector]],
+            isClosed=False,
+            color=robot_color,
+            thickness=int(backbone_thicknesses[segment_idx].item()),
+        )
 
     return img
