93d13a1a8712d6cdb5f64bf4366f4d77846c9b79
Maximilian Stölzle <maximilian@stoelzle.ch>
2025-12-21 11:42:19 +0100

    Add ClosedFormModelBasedController class and update __all__ exports

--- src/soromox/control/__init__.py
+++ src/soromox/control/__init__.py
@@ -1,4 +1,5 @@
 from .base_controller import BaseController
+from .closed_form_model_based_controller import ClosedFormModelBasedController
 from .reference_trajectory import ReferenceTrajectory
 
-__all__ = ["BaseController", "ReferenceTrajectory"]
+__all__ = ["BaseController", "ClosedFormModelBasedController", "ReferenceTrajectory"]
