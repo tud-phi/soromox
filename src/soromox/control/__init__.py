5c95fec8c0e96d588e34c45be5f54a99e023c684
Maximilian Stölzle <maximilian@stoelzle.ch>
2025-12-21 12:17:55 +0100

    Add PIDControl class for PID control implementation and update __all__ exports

--- src/soromox/control/__init__.py
+++ src/soromox/control/__init__.py
@@ -1,5 +1,11 @@
 from .base_controller import BaseController
 from .closed_form_model_based_controller import ClosedFormModelBasedController
+from .pid_control import PIDControl
 from .reference_trajectory import ReferenceTrajectory
 
-__all__ = ["BaseController", "ClosedFormModelBasedController", "ReferenceTrajectory"]
+__all__ = [
+    "BaseController",
+    "ClosedFormModelBasedController",
+    "PIDControl",
+    "ReferenceTrajectory",
+]
