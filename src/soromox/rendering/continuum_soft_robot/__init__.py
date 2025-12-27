2364963b62bd17cb5a7096bce07bf1e239c2b216
Maximilian Stölzle <maximilian@stoelzle.ch>
2025-12-27 15:26:09 +0100

    Remove compatibility re-exports for moved renderers in __init__.py

--- src/soromox/rendering/continuum_soft_robot/__init__.py
+++
@@ -1,6 +0,0 @@
-"""Compatibility re-exports for moved renderers."""
-
-from soromox.rendering.matplotlib_renderer import MatplotlibRenderer
-from soromox.rendering.open3d_renderer import Open3DRenderer
-
-__all__ = ["MatplotlibRenderer", "Open3DRenderer"]
