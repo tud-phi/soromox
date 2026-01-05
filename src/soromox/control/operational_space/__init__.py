8ed2098699f35b036129061feff15f5512247f90
Maximilian Stölzle <maximilian@stoelzle.ch>
2026-01-05 20:35:55 +0100

    Add SynergisticController to module exports

--- src/soromox/control/operational_space/__init__.py
+++ src/soromox/control/operational_space/__init__.py
@@ -1,8 +1,9 @@
 from .base_controller import OperationalSpaceBaseController
 from .impedance_control_tracker import ImpedanceControlTracker
+from .synergistic_controller import SynergisticController
 
 __all__ = [
     "OperationalSpaceBaseController",
     "ImpedanceControlTracker",
+    "SynergisticController",
 ]
-
