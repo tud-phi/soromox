e860bba7fba24fd24d0064fe71863da508ef4c49
Danielfeliutalegon <danielfeliu91@hotmail.com>
2026-06-15 19:40:19 +0200

    Added data bechmarking
    Added the results and files of the bechmarking of the paper

---
+++ examples/simulation/Benchmarking/Script_Validation_Complex_GVS.m
@@ -0,0 +1,57 @@
+clear all;
+close all;
+clc;
+
+addpath('Data_Simulations')
+
+FontLabels      = 32;
+% axes1 = axes('FontSize',34);box(axes1,'on');hold(axes1,'on');
+set(groot, 'defaultAxesFontName', 'Times New Roman');
+set(groot, 'defaultAxesFontSize', FontLabels);
+set(groot, 'defaultTextInterpreter', 'latex');
+set(groot, 'defaultLegendInterpreter', 'latex');
+set(groot, 'defaultAxesTickLabelInterpreter', 'latex');
+
+colors = [0 0.6 1;  % blue
+    0.55 0.27 0.07;
+    0.4660 0.6740 0.1880;  % green
+    1 0.1 0.1];
+
+
+%%
+kk=1;
+
+load("end_effector_position_Complex_GVS_SoRoSim.mat")
+plot(t(1:kk:end),Pos_EE_all(1,(1:kk:end)),'LineWidth',3,'Color', colors(1,:));hold on;grid on;
+plot(t(1:kk:end),Pos_EE_all(2,(1:kk:end)),'LineWidth',3,'Color', colors(2,:));hold on;grid on;
+plot(t(1:kk:end),Pos_EE_all(3,(1:kk:end)),'LineWidth',3,'Color', colors(3,:));hold on;grid on;
+xlabel('Time $ (\mathrm{s})$','Interpreter','latex','FontName','Times New Roman','FontSize',FontLabels);
+ylabel('Tip Coordinates $(\mathrm{m})$','Interpreter','latex','FontName','Times New Roman','FontSize',FontLabels);
+% set(gca, 'XTickLabel', []);
+
+% kk=20;
+
+T = readtable("end_effector_position_Spatial_PyElastica.xlsx");
+kkk=15;
+hold on;plot(T.time(1:kk:end),T.x(1:kk:end),'LineWidth',4,'Color', colors(1,:),'LineStyle','--');hold on;
+hold on;plot(T.time(1:kk:end),T.y(1:kk:end),'LineWidth',4,'Color', colors(2,:),'LineStyle','--');hold on;
+plot(T.time(1:kk:end),T.z(1:kk:end),'LineWidth',4,'Color', colors(3,:),'LineStyle','--');
+
+
+% kk=10;
+
+T1 = readtable("end_effector_position_Complex_GVS_SoRoMoX.csv");
+kkk=10;
+
+hold on;plot(T1.t(1:kk:end),T1.x(1:kk:end),'LineWidth',4,'Color', colors(1,:),'LineStyle','-.');hold on;
+plot(T1.t(1:kk:end),T1.y(1:kk:end),'LineWidth',4,'Color', colors(2,:),'LineStyle','-.');hold on;
+plot(T1.t(1:kk:end),T1.z(1:kk:end),'LineWidth',4,'Color', colors(3,:),'LineStyle','-.');
+
+legend({'$x_t$ (SoRoSim)', '$y_t$ (SoRoSim)','$z_t$ (SoRoSim)', ...
+    '$x_t$ (PyElastica)','$y_t$ (PyElastica)','$z_t$ (PyElastica)', ...
+                '$x_t$ (SoRoMoX)','$y_t$ (SoRoMoX)','$z_t$ (SoRoMoX)'}, ...               
+        'Interpreter','latex', ...
+        'Location','north', ...
+        'Orientation','horizontal', ...
+        'NumColumns',3);
+
---
+++ examples/simulation/Benchmarking/Scripts_Sorosim/Script_Simulation_SoRoSim.m
@@ -0,0 +1,90 @@
+startup
+
+clear all;
+close all;
+clc;
+
+FontLabels      = 32;
+set(groot, 'defaultAxesFontName', 'Times New Roman');
+set(groot, 'defaultAxesFontSize', FontLabels);
+set(groot, 'defaultTextInterpreter', 'latex');
+set(groot, 'defaultLegendInterpreter', 'latex');
+set(groot, 'defaultAxesTickLabelInterpreter', 'latex');
+
+colors = [0 0.6 1;  % blue
+    0.55 0.27 0.07;
+    0.4660 0.6740 0.1880;  % green
+    1 0.1 0.1];
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% I RUN THE SELECTED CASE: Case = 1: Planar PCS, Case = 2: Spatial PCS,
+% Case = 3: Complex GVS, Case 4: Tendon-driven GVS
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+Case = 4;
+
+switch Case
+   case 1
+      load 'System_Planar_PCS.mat';
+   case 2
+      load 'System_Spatial_PCS.mat';
+   case 3
+      load 'System_Complex_GVS.mat';
+   otherwise
+      load 'System_Tendon_driven_GVS.mat';
+end
+
+S1.PlotParameters.CameraPosition = [0 -6.235382907247957 0];
+S1.PlotParameters.CameraTarget = [0 0 0];
+S1.PlotParameters.CameraUpVector = [0 0 1];
+
+% Simulation seettings
+
+tmax                    = 3;
+h                       = 0.0001;
+odetype                 = 'ode45'; 
+global gv;
+gv.tmax                 = tmax;
+qqd0      = zeros(2*S1.ndof,1);
+
+tic
+
+[t,qqd] = dynamics(S1,qqd0,odetype,h);
+
+% S1.plotqqd(t,qqd)
+time_comp = toc;
+
+
+fprintf("Computation time: %.6f s\n", time_comp);
+
+%%
+Pos_EE_all  = [];
+for i=1:length(t)    
+    q_here                  = qqd(i,1:end/2);
+    g_all                   = FwdKinematics(S1,q_here);
+    g_EE                    = g_all(end-3:end,end-3:end);
+    Pos_EE                  = g_EE(1:3,4);
+    Pos_EE_all              = [Pos_EE_all Pos_EE];
+end
+
+
+%%
+
+plot(t,Pos_EE_all(1,:),'LineWidth',3,'Color', colors(1,:));hold on;grid on;
+plot(t,Pos_EE_all(2,:),'LineWidth',3,'Color', colors(2,:));hold on;grid on;
+plot(t,Pos_EE_all(3,:),'LineWidth',3,'Color', colors(3,:));hold on;grid on;
+xlabel('Time $(\mathrm{s})$','Interpreter','latex','FontName','Times New Roman','FontSize',FontLabels);
+ylabel('Tip Coordinates $(\mathrm{m})$','Interpreter','latex','FontName','Times New Roman','FontSize',FontLabels);
+
+legend({'$x_t$ (SoRoSim)', '$y_t$ (SoRoSim)','$z_t$ (SoRoSim)'}, ...
+        'Interpreter','latex', ...
+        'Location','north', ...
+        'Orientation','horizontal', ...
+        'NumColumns',3);
+
+
+
+
+
+
+
