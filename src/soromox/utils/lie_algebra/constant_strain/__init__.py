56d00c1d2755a85e2c725e53048cc8b9f5733b2f
Maximilian Stölzle <maximilian@stoelzle.ch>
2026-08-16 21:00:55 -0400

    Tighten constant-strain package surface

--- src/soromox/utils/lie_algebra/constant_strain/__init__.py
+++ src/soromox/utils/lie_algebra/constant_strain/__init__.py
@@ -1,9 +1,4 @@
-r"""Closed-form Lie operators for constant-strain rod segments.
-
-The algebra-specific :mod:`.se2` and :mod:`.se3` namespaces provide concise
-public functions for planar and spatial rod kinematics. Both modules return
-:class:`ConstantStrainOperators` from their bundled ``operators`` evaluator.
-"""
+r"""Closed-form Lie operators for constant-strain rod segments."""
 
 from . import se2, se3
 from ._shared import ConstantStrainOperators
